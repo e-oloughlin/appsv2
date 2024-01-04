fe25b408ed1a572429095d486ad870a9fd586b6a
Eoghan <eoghan.oloughlin@genesys.com>
2024-01-04 10:08:18 +0000

    Use beta version of host library and show two apps in app pool

--- src/appsv2.ts
+++ src/appsv2.ts
@@ -1,5 +1,8 @@
-import { AppPool, AppDef } from '@genesys/apps-v2-host';
+import { AppPool, AppDef, IframeAppDef } from '@genesys/apps-v2-host';
 
+// Could be a conversation ID
+// or random UUID maybe
+// could also be something like "main" for single groups
 export const groupID = "4f09f81b-04f3-4646-8f3f-0b11c873ef5d";
 
 export const appPool = new AppPool({}, {
@@ -16,10 +19,10 @@ const downloadChromeBrowserAppDef: AppDef = {
   src: new URL('https://www.google.com/intl/en_US/chrome/'),
   sandbox: ['allow-scripts', 'allow-same-origin', 'allow-forms'],
   startBehavior: 'lazy',
-  // monochromicIcon: {
-  //   type: 'sparkV3SvgSprite',
-  //   classNames: ['chrome']
-  // },
+  monochromicIcon: {
+    type: 'sparkV3SvgSprite',
+    classNames: ['chrome']
+  },
   icon: {
     type: 'remoteVectorImage',
     url: new URL(
@@ -28,6 +31,37 @@ const downloadChromeBrowserAppDef: AppDef = {
   }
 };
 
+export const appsSdkShowcaseIframeApp: IframeAppDef = {
+  id: 'appsSdkShowcaseIframeApp',
+  displayName: 'Apps SDK Showcase (Public)',
+  type: 'iframe',
+  sandbox: [
+      'allow-same-origin',
+      'allow-scripts',
+      'allow-modals',
+      'allow-popups',
+      'allow-forms',
+      'allow-downloads'
+  ],
+  src: new URL(
+      'https://apps.inindca.com/appsv2-demo/apps/apps-sdk-showcase/public.html',
+      window.location.origin + window.location.pathname
+  ),
+  monochromicIcon: {
+      type: 'sparkV3SvgSprite',
+      classNames: ['fa/code-regular']
+  },
+  startBehavior: 'lazy',
+  trustedDataPassing: true,
+  lifecycleConfig: {
+      bootstrap: true,
+      focus: true,
+      blur: true,
+      stop: true
+  }
+};
+
+// this is an alternative to passing app defs on instantiation
 appPool.updateAppDefs({
-  [groupID]: [downloadChromeBrowserAppDef]
+  [groupID]: [downloadChromeBrowserAppDef, appsSdkShowcaseIframeApp],
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+import { registerElements } from 'genesys-spark-components';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'genesys-spark-components/dist/genesys-webcomponents/genesys-webcomponents.css';
 import App from './App';
+
+registerElements();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
